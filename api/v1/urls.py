e1d9dee7d76cf47d6776d1440f1efcba44293a47
webersni <nicolas.webersinke@outlook.com>
2020-07-30 17:45:57 +0200

    Change routes to more meaningful names

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -5,18 +5,18 @@ from . import api
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'dailyecbriskfreerate', api.DailyECBRiskFreeRateViewSet)
-router.register(r'monthlyecbriskfreerate', api.MonthlyECBRiskFreeRateViewSet)
-router.register(r'annuallyecbriskfreerate', api.AnnuallyECBRiskFreeRateViewSet)
-router.register(r'dailyexchangerateusdperx', api.DailyExchangeRateUSDPerXViewSet)
-router.register(r'monthlyexchangerateusdperx', api.MonthlyExchangeRateUSDPerXViewSet)
-router.register(r'annuallyexchangerateusdperx', api.AnnuallyExchangeRateUSDPerXViewSet)
-router.register(r'dailythreefactor', api.DailyThreeFactorViewSet)
-router.register(r'monthlythreefactor', api.MonthlyThreeFactorViewSet)
-router.register(r'yearlythreefactor', api.YearlyThreeFactorViewSet)
-router.register(r'dailyfivefactor', api.DailyFiveFactorViewSet)
-router.register(r'monthlyfivefactor', api.MonthlyFiveFactorViewSet)
-router.register(r'annuallyfivefactor', api.AnnuallyFiveFactorViewSet)
+router.register(r'eurrf/daily', api.DailyECBRiskFreeRateViewSet)
+router.register(r'eurrf/monthly', api.MonthlyECBRiskFreeRateViewSet)
+router.register(r'eurrf/annually', api.AnnuallyECBRiskFreeRateViewSet)
+router.register(r'fxrate/daily', api.DailyExchangeRateUSDPerXViewSet)
+router.register(r'fxrate/monthly', api.MonthlyExchangeRateUSDPerXViewSet)
+router.register(r'fxrate/annually', api.AnnuallyExchangeRateUSDPerXViewSet)
+router.register(r'3factor/daily', api.DailyThreeFactorViewSet)
+router.register(r'3factor/monthly', api.MonthlyThreeFactorViewSet)
+router.register(r'3factor/annually', api.YearlyThreeFactorViewSet)
+router.register(r'5factor/daily', api.DailyFiveFactorViewSet)
+router.register(r'5factor/monthly', api.MonthlyFiveFactorViewSet)
+router.register(r'5factor/annually', api.AnnuallyFiveFactorViewSet)
 
 
 urlpatterns = (
