31485a5143881a7f82bf89924f62a99f0d9dac42
webersni <nicolas.webersinke@outlook.com>
2020-08-19 15:58:31 +0200

    Refactor French link config

---
+++ api/french_config.py
@@ -0,0 +1,144 @@
+"""
+Config file for Kenneth French's data library links
+"""
+
+regions = [
+    {
+        "name": "Developed",
+        "freq": [
+            {
+                "interval": "D",
+                "3factors": "Developed_3_Factors_Daily_CSV",
+                "5factors": "Developed_5_Factors_Daily_CSV",
+                "mom": "Developed_MOM_Factor_Daily_CSV",
+            },
+            {
+                "interval": "M",
+                "3factors": "Developed_3_Factors_CSV",
+                "5factors": "Developed_5_Factors_CSV",
+                "mom": "Developed_MOM_Factor_CSV",
+            },
+            {
+                "interval": "A",
+                "3factors": "Developed_3_Factors_CSV",
+                "5factors": "Developed_5_Factors_CSV",
+                "mom": "Developed_MOM_Factor_CSV",
+            },
+        ],
+    },
+    {
+        "name": "Developed_ex_US",
+        "freq": [
+            {
+                "interval": "D",
+                "3factors": "Developed_ex_US_3_Factors_Daily_CSV",
+                "5factors": "Developed_ex_US_5_Factors_Daily_CSV",
+                "mom": "Developed_ex_US_MOM_Factor_Daily_CSV",
+            },
+            {
+                "interval": "M",
+                "3factors": "Developed_ex_US_3_Factors_CSV",
+                "5factors": "Developed_ex_US_5_Factors_CSV",
+                "mom": "Developed_ex_US_MOM_Factor_CSV",
+            },
+            {
+                "interval": "A",
+                "3factors": "Developed_ex_US_3_Factors_CSV",
+                "5factors": "Developed_ex_US_5_Factors_CSV",
+                "mom": "Developed_ex_US_MOM_Factor_CSV",
+            },
+        ],
+    },
+    {
+        "name": "Europe",
+        "freq": [
+            {
+                "interval": "D",
+                "3factors": "Europe_3_Factors_Daily_CSV",
+                "5factors": "Europe_5_Factors_Daily_CSV",
+                "mom": "Europe_MOM_Factor_Daily_CSV",
+            },
+            {
+                "interval": "M",
+                "3factors": "Europe_3_Factors_CSV",
+                "5factors": "Europe_5_Factors_CSV",
+                "mom": "Europe_MOM_Factor_CSV",
+            },
+            {
+                "interval": "A",
+                "3factors": "Europe_3_Factors_CSV",
+                "5factors": "Europe_5_Factors_CSV",
+                "mom": "Europe_MOM_Factor_CSV",
+            },
+        ],
+    },
+    {
+        "name": "Japan",
+        "freq": [
+            {
+                "interval": "D",
+                "3factors": "Japan_3_Factors_Daily_CSV",
+                "5factors": "Japan_5_Factors_Daily_CSV",
+                "mom": "Japan_MOM_Factor_Daily_CSV",
+            },
+            {
+                "interval": "M",
+                "3factors": "Japan_3_Factors_CSV",
+                "5factors": "Japan_5_Factors_CSV",
+                "mom": "Japan_MOM_Factor_CSV",
+            },
+            {
+                "interval": "A",
+                "3factors": "Japan_3_Factors_CSV",
+                "5factors": "Japan_5_Factors_CSV",
+                "mom": "Japan_MOM_Factor_CSV",
+            },
+        ],
+    },
+    {
+        "name": "Asia_Pacific_ex_Japan",
+        "freq": [
+            {
+                "interval": "D",
+                "3factors": "Asia_Pacific_ex_Japan_3_Factors_Daily_CSV",
+                "5factors": "Asia_Pacific_ex_Japan_5_Factors_Daily_CSV",
+                "mom": "Asia_Pacific_ex_Japan_MOM_Factor_Daily_CSV",
+            },
+            {
+                "interval": "M",
+                "3factors": "Asia_Pacific_ex_Japan_3_Factors_CSV",
+                "5factors": "Asia_Pacific_ex_Japan_5_Factors_CSV",
+                "mom": "Asia_Pacific_ex_Japan_MOM_Factor_CSV",
+            },
+            {
+                "interval": "A",
+                "3factors": "Asia_Pacific_ex_Japan_3_Factors_CSV",
+                "5factors": "Asia_Pacific_ex_Japan_5_Factors_CSV",
+                "mom": "Asia_Pacific_ex_Japan_MOM_Factor_CSV",
+            },
+        ],
+    },
+    {
+        "name": "North_America",
+        "freq": [
+            {
+                "interval": "D",
+                "3factors": "North_America_3_Factors_Daily_CSV",
+                "5factors": "North_America_5_Factors_Daily_CSV",
+                "mom": "North_America_MOM_Factor_Daily_CSV",
+            },
+            {
+                "interval": "M",
+                "3factors": "North_America_3_Factors_CSV",
+                "5factors": "North_America_5_Factors_CSV",
+                "mom": "North_America_MOM_Factor_CSV",
+            },
+            {
+                "interval": "A",
+                "3factors": "North_America_3_Factors_CSV",
+                "5factors": "North_America_5_Factors_CSV",
+                "mom": "North_America_MOM_Factor_CSV",
+            },
+        ],
+    },
+]
