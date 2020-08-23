50b5aa1e648d69a159f650d0b8635f7aba4cf93d
webersni <nicolas.webersinke@outlook.com>
2020-08-23 23:10:20 +0200

    Add throttling and token-based authentication

--- api/v1/api.py
+++ api/v1/api.py
@@ -1,6 +1,7 @@
-from rest_framework import generics
-from rest_framework.exceptions import ValidationError
+from rest_framework import generics, permissions
+from rest_framework.exceptions import ValidationError, Throttled
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
 
 from . import models, serializers
 
@@ -45,6 +46,14 @@ regions = [
 
 def errorhandler(message):
     raise ValidationError({"Error": message})
+
+
+def throttle_handler(wait):
+    raise Throttled(
+        detail={
+            "Error": f"Request limit exceeded. Reset in {int(wait/60)} min. Create a user account and get your token or contact us for further options.",
+        }
+    )
 
 
 def factors_validate_and_filter(obj, factor_model):
@@ -110,6 +119,26 @@ def factors_validate_and_filter(obj, factor_model):
     return objects.exclude(**filter_fields)
 
 
+class QueryTokenAuthentication(TokenAuthentication):
+    """
+    Extend the TokenAuthentication class to support authentication
+    via token parameter in querystring.
+    """
+
+    def authenticate(self, request):
+        token = request.query_params.get("token")
+
+        if token:
+            return self.authenticate_credentials(token.strip())
+
+        return None
+
+
+class ReadOnlyAPI(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
+
+
 ### Views ###
 
 ### Risk Free Rate ###
@@ -119,6 +148,8 @@ class RiskFreeRateView(generics.ListAPIView):
     """View for the RiskFreeRate class"""
 
     serializer_class = serializers.RiskFreeRateSerializer
+    permission_classes = [permissions.IsAdminUser | ReadOnlyAPI]
+    authentication_classes = [QueryTokenAuthentication]
 
     def get_queryset(self):
         currency = self.kwargs["currency"]
@@ -160,12 +191,18 @@ class RiskFreeRateView(generics.ListAPIView):
 
         return objects.exclude(RF__isnull=True)
 
+    def throttled(self, request, wait):
+        throttle_handler(wait)
+
 
 ### Exchange Rates ###
 
 
 class ExchangeRateUSDPerXView(generics.ListAPIView):
     """View for the DailyExchangeRateUSDPerX class"""
+
+    permission_classes = [permissions.IsAdminUser | ReadOnlyAPI]
+    authentication_classes = [QueryTokenAuthentication]
 
     # Overwrite list method to make use of dynamic serializer
     def list(self, request, currency, interval):
@@ -215,12 +252,18 @@ class ExchangeRateUSDPerXView(generics.ListAPIView):
 
         return objects.exclude(**{currency + "__isnull": True})
 
+    def throttled(self, request, wait):
+        throttle_handler(wait)
+
 
 ### Factor returns ###
 
 
 class ThreeFactorView(generics.ListAPIView):
     """View for the ThreeFactor class"""
+
+    permission_classes = [permissions.IsAdminUser | ReadOnlyAPI]
+    authentication_classes = [QueryTokenAuthentication]
 
     def list(self, request, factor, region, currency, interval):
         queryset = self.get_queryset()
@@ -237,9 +280,15 @@ class ThreeFactorView(generics.ListAPIView):
     def get_queryset(self):
         return factors_validate_and_filter(self, 3)
 
+    def throttled(self, request, wait):
+        throttle_handler(wait)
+
 
 class FourFactorView(generics.ListAPIView):
     """View for the DailyFourFactor class"""
+
+    permission_classes = [permissions.IsAdminUser | ReadOnlyAPI]
+    authentication_classes = [QueryTokenAuthentication]
 
     def list(self, request, factor, region, currency, interval):
         queryset = self.get_queryset()
@@ -256,9 +305,15 @@ class FourFactorView(generics.ListAPIView):
     def get_queryset(self):
         return factors_validate_and_filter(self, 4)
 
+    def throttled(self, request, wait):
+        throttle_handler(wait)
+
 
 class FiveFactorView(generics.ListAPIView):
     """View for the DailyFiveFactor class"""
+
+    permission_classes = [permissions.IsAdminUser | ReadOnlyAPI]
+    authentication_classes = [QueryTokenAuthentication]
 
     def list(self, request, factor, region, currency, interval):
         queryset = self.get_queryset()
@@ -275,9 +330,15 @@ class FiveFactorView(generics.ListAPIView):
     def get_queryset(self):
         return factors_validate_and_filter(self, 5)
 
+    def throttled(self, request, wait):
+        throttle_handler(wait)
+
 
 class SixFactorView(generics.ListAPIView):
     """View for the DailySixFactor class"""
+
+    permission_classes = [permissions.IsAdminUser | ReadOnlyAPI]
+    authentication_classes = [QueryTokenAuthentication]
 
     def list(self, request, factor, region, currency, interval):
         queryset = self.get_queryset()
@@ -293,3 +354,6 @@ class SixFactorView(generics.ListAPIView):
 
     def get_queryset(self):
         return factors_validate_and_filter(self, 6)
+
+    def throttled(self, request, wait):
+        throttle_handler(wait)
