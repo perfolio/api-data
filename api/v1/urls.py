706a6893e3e180be7f55019eb6f60fe8a973dc54
webersni <nicolas.webersinke@outlook.com>
2020-08-23 18:04:15 +0200

    Change urlpatterns from router-based to path-based

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,63 +1,27 @@
-from django.urls import include, path
-from rest_framework import routers
-
-from . import api, views
-
-router = routers.DefaultRouter()
-router.register(r"rf/(?P<currency>.+)/daily", api.DailyRiskFreeRateViewSet)
-router.register(r"rf/(?P<currency>.+)/monthly", api.MonthlyRiskFreeRateViewSet)
-router.register(
-    r"rf/(?P<currency>.+)/annually", api.AnnuallyRiskFreeRateViewSet
-)
-router.register(
-    r"fxrate/(?P<currency>.+)/daily", api.DailyExchangeRateUSDPerXViewSet
-)
-router.register(
-    r"fxrate/(?P<currency>.+)/monthly", api.MonthlyExchangeRateUSDPerXViewSet
-)
-router.register(
-    r"fxrate/(?P<currency>.+)/annually", api.AnnuallyExchangeRateUSDPerXViewSet
-)
-router.register(r"3factor/(?P<currency>.+)/daily", api.DailyThreeFactorViewSet)
-router.register(
-    r"3factor/(?P<currency>.+)/monthly", api.MonthlyThreeFactorViewSet
-)
-router.register(
-    r"3factor/(?P<currency>.+)/annually", api.AnnuallyThreeFactorViewSet
-)
-router.register(r"4factor/(?P<currency>.+)/daily", api.DailyFourFactorViewSet)
-router.register(
-    r"4factor/(?P<currency>.+)/monthly", api.MonthlyFourFactorViewSet
-)
-router.register(
-    r"4factor/(?P<currency>.+)/annually", api.AnnuallyFourFactorViewSet
-)
-router.register(r"5factor/(?P<currency>.+)/daily", api.DailyFiveFactorViewSet)
-router.register(
-    r"5factor/(?P<currency>.+)/monthly", api.MonthlyFiveFactorViewSet
-)
-router.register(
-    r"5factor/(?P<currency>.+)/annually", api.AnnuallyFiveFactorViewSet
-)
-router.register(r"6factor/(?P<currency>.+)/daily", api.DailySixFactorViewSet)
-router.register(
-    r"6factor/(?P<currency>.+)/monthly", api.MonthlySixFactorViewSet
-)
-router.register(
-    r"6factor/(?P<currency>.+)/annually", api.AnnuallySixFactorViewSet
-)
-router.register(
-    r"factor/(?P<factor>.+)/(?P<currency>.+)/daily", api.DailyFactorViewSet
-)
-router.register(
-    r"factor/(?P<factor>.+)/(?P<currency>.+)/monthly", api.MonthlyFactorViewSet
-)
-router.register(
-    r"factor/(?P<factor>.+)/(?P<currency>.+)/annually",
-    api.AnnuallyFactorViewSet,
-)
+from django.urls import path
+from . import api
 
 urlpatterns = (
     # urls for Django Rest Framework API
-    path("", include(router.urls)),
+    path("rf/<str:currency>/<str:interval>", api.RiskFreeRateView.as_view(),),
+    path(
+        "fxrate/<str:currency>/<str:interval>",
+        api.ExchangeRateUSDPerXView.as_view(),
+    ),
+    path(
+        "3factor/<str:factor>/<str:region>/<str:currency>/<str:interval>",
+        api.ThreeFactorView.as_view(),
+    ),
+    path(
+        "4factor/<str:factor>/<str:region>/<str:currency>/<str:interval>",
+        api.FourFactorView.as_view(),
+    ),
+    path(
+        "5factor/<str:factor>/<str:region>/<str:currency>/<str:interval>",
+        api.FiveFactorView.as_view(),
+    ),
+    path(
+        "6factor/<str:factor>/<str:region>/<str:currency>/<str:interval>",
+        api.SixFactorView.as_view(),
+    ),
 )
