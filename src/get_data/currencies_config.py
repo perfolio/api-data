71bea10b3bd5943fa1794d1d7ff8099f986e8d65
webersni <nicolas.webersinke@outlook.com>
2020-08-30 21:37:04 +0200

    Refactor currencies config

---
+++ src/get_data/currencies_config.py
@@ -0,0 +1,24 @@
+# Add supported currencies for fx rates here
+currencies_fxrates = [
+    "EUR",
+    "JPY",
+    "GBP",
+    "CHF",
+    "RUB",
+    "AUD",
+    "BRL",
+    "CAD",
+    "CNY",
+    "INR",
+    "DKK",
+    "NZD",
+    "NOK",
+    "SEK",
+    "PLN",
+    "ILS",
+    "KRW",
+    "TRY",
+]
+
+# Add supported currencies for riskfree rates here
+currencies_rf = ["USD", "EUR"]
