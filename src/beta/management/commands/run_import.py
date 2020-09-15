6fa00600efa705c3b4905c5172476ca09d019597
webersni <nicolas.webersinke@outlook.com>
2020-09-15 22:54:59 +0200

    Add factor return data import to command

--- src/beta/management/commands/run_import.py
+++ src/beta/management/commands/run_import.py
@@ -1,9 +1,12 @@
 import logging
 from typing import Any, Optional
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
 from beta import models
+from get_data.config.french import regions
+from get_data.config.general import currencies_fxrates
+from get_data.convert import FactorConverter
 from get_data.fetch import Fetcher as fe
 
 
@@ -17,29 +20,100 @@ class Command(BaseCommand):
         self.stdout.write(
             "Starting data import of factor returns, riskfree rates and exchange rates..."
         )
-        """
+
         # ECB Risk Free Rates #
-        df_rf_eur_d = fe.ecb_riskfreerates("daily")
-        df_rf_eur_m = fe.ecb_riskfreerates("monthly")
-        df_rf_eur_a = fe.ecb_riskfreerates("annual")
 
-        models.RiskFreeRate.from_dataframe(df_rf_eur_d, "daily", "EUR")
-        models.RiskFreeRate.from_dataframe(df_rf_eur_m, "monthly", "EUR")
-        models.RiskFreeRate.from_dataframe(df_rf_eur_a, "annual", "EUR")
+        dict_rf = {
+            "daily": {"EUR": fe.ecb_riskfreerates("daily")},
+            "monthly": {"EUR": fe.ecb_riskfreerates("monthly")},
+            "annual": {"EUR": fe.ecb_riskfreerates("annual")},
+        }
 
-        # Possibly add riskfree rates for other currencies here
-        dict_rf_d = {"EUR": df_rf_eur_d}
-        dict_rf_m = {"EUR": df_rf_eur_m}
-        dict_rf_a = {"EUR": df_rf_eur_a}
+        models.RiskFreeRate.from_dataframe(dict_rf["daily"]["EUR"], "daily", "EUR")
+        models.RiskFreeRate.from_dataframe(dict_rf["monthly"]["EUR"], "monthly", "EUR")
+        models.RiskFreeRate.from_dataframe(dict_rf["annual"]["EUR"], "annual", "EUR")
 
         # BOE Exchange Rates USD per X #
 
-        df_fxrates_d = fe.boe_fxrates("daily")
-        df_fxrates_m = fe.boe_fxrates("monthly")
-        df_fxrates_a = fe.boe_fxrates("annual")
+        dict_fxrates = {
+            "daily": fe.boe_fxrates("daily"),
+            "monthly": fe.boe_fxrates("monthly"),
+            "annual": fe.boe_fxrates("annual"),
+        }
 
-        models.ExchangeRateUSDPerX.from_dataframe(df_fxrates_d, "daily")
-        models.ExchangeRateUSDPerX.from_dataframe(df_fxrates_m, "monthly")
-        models.ExchangeRateUSDPerX.from_dataframe(df_fxrates_a, "annual")
-        """
+        models.ExchangeRateUSDPerX.from_dataframe(dict_fxrates["daily"], "daily")
+        models.ExchangeRateUSDPerX.from_dataframe(dict_fxrates["monthly"], "monthly")
+        models.ExchangeRateUSDPerX.from_dataframe(dict_fxrates["annual"], "annual")
+
+        # Kenneth French factor returns #
+
+        fc = {}
+
+        for region in regions:
+            self.stdout.write(f"Parsing factor returns for region {region['name']}...")
+
+            # Check if region has 3 factor data and set flag for parsing
+            try:
+                if region["freq"][0]["3factors"]:
+                    three_factor = True
+                else:
+                    three_factor = False
+            except KeyError:
+                three_factor = False
+
+            for f in region["freq"]:
+
+                df_mom_usd = fe.french_factors(f["mom"], f["interval"])
+
+                if three_factor:
+                    df_3n4factors_usd = fe.french_factors(f["3factors"], f["interval"])
+
+                    if region["name"] == "usa":
+                        dict_rf[f["interval"]]["USD"] = df_3n4factors_usd[["rf"]]
+
+                        models.RiskFreeRate.from_dataframe(
+                            dict_rf[f["interval"]]["USD"], f["interval"], "USD"
+                        )
+
+                        fc[f["interval"]] = FactorConverter(
+                            dict_fxrates[f["interval"]], dict_rf[f["interval"]]
+                        )
+
+                    df_3n4factors_usd.drop(["rf"], axis=1, inplace=True)
+                    df_3n4factors_usd["mom"] = df_mom_usd["mom"]
+
+                df_5n6factors_usd = fe.french_factors(f["5factors"], f["interval"])
+
+                df_5n6factors_usd.drop(["rf"], axis=1, inplace=True)
+                df_5n6factors_usd["mom"] = df_mom_usd["mom"]
+
+                if three_factor:
+                    models.ThreeFourFactor.from_dataframe(
+                        df_3n4factors_usd, f["interval"], "USD", region["name"]
+                    )
+
+                    for currency in currencies_fxrates:
+                        df_converted = fc[f["interval"]].dataframe(
+                            df_3n4factors_usd, currency
+                        )
+
+                        models.ThreeFourFactor.from_dataframe(
+                            df_converted, f["interval"], currency, region["name"]
+                        )
+
+                models.FiveSixFactor.from_dataframe(
+                    df_5n6factors_usd, f["interval"], "USD", region["name"]
+                )
+
+                for currency in currencies_fxrates:
+                    df_converted = fc[f["interval"]].dataframe(
+                        df_5n6factors_usd, currency
+                    )
+
+                    models.FiveSixFactor.from_dataframe(
+                        df_converted, f["interval"], currency, region["name"]
+                    )
+
+        self.stdout.write("Finished data import successfully!")
+
         return None
